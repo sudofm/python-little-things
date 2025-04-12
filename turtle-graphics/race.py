0d81461a3ca7c9f064884d186ca3541a9a51f027
faciquidoumbia <fmdoums14@gmail.com>
2025-04-12 15:40:39 +0200

    race in fr

--- turtle-graphics/race.py
+++ turtle-graphics/race.py
@@ -1,13 +1,48 @@
-import turtle
 from turtle import Turtle, Screen
 import turtle as t
 import random
 
+
+def get_color_fr(color):
+    match color:
+        case "red":
+            return "rouge"
+        case "orange":
+            return "orange"
+        case "yellow":
+            return "jaune"
+        case "green":
+            return "verte"
+        case "blue":
+            return "bleu"
+        case "indigo":
+            return "indigo"
+        case "purple":
+            return "violete"
+
+def get_color_en(color):
+    match color:
+        case "rouge":
+            return "red"
+        case "orange":
+            return "orange"
+        case "jaune":
+            return "yellow"
+        case "verte":
+            return "yellow"
+        case "bleu":
+            return "blue"
+        case "indigo":
+            return "indigo"
+        case "violette":
+            return "purple"
+
+
+
 screen = Screen()
 screen.setup(width=900, height=600)
-user_bet = screen.textinput(title="Faites un choix", prompt="Quelle tortue remportera la course ? Choisissez une couleur: ")
+user_bet = get_color_en(screen.textinput(title="Faites un choix", prompt="Quelle tortue remportera la course ? Choisissez une couleur: "))
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
-#colors = ['rouge', 'orange', 'jaune', 'vert', 'bleu', 'indigo', 'violet']
 t.colormode(255)
 is_race_on = False
 all_turtles = []
@@ -31,9 +66,9 @@ while is_race_on:
             is_race_on = False
             winning_color = turtle.pencolor()
             if winning_color == user_bet:
-                print(f"Vous avez gagné ! La tortue {winning_color} est la tortue gagnante !")
+                print(f"Vous avez gagné ! La tortue {get_color_fr(winning_color)} est la tortue gagnante !")
             else:
-                print(f"Vous avez perdu ! La tortue {winning_color} est la tortue gagnante !")
+                print(f"Vous avez perdu ! La tortue {get_color_fr(winning_color)} est la tortue gagnante !")
 
         rand_distance = random.randint(0, 10)
         turtle.forward(rand_distance)
