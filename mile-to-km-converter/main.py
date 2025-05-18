0775c9a15aa56554145877216a0123705fb3ebb3
faciquidoumbia <fmdoums14@gmail.com>
2025-05-18 11:27:54 +0200

    miles-to-km converter

--- mile-to-km-converter/main.py
+++ mile-to-km-converter/main.py
@@ -1,35 +1,35 @@
 from tkinter import *
 
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    km_result_label.config(text=f"{km}")
+
 window = Tk()
 window.title("Mile to Km Converter")
-window.minsize(width=400, height=200)
+window.config(padx=20, pady=20)
 
-#Label 1
-label_1 = Label(text="is equal to")
-label_1.grid(column=1, row=2)
 
-#Label 2
+#Labels
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
 label_miles = Label(text="Miles")
-label_miles.grid(column=3, row=1)
+label_miles.grid(column=2, row=0)
 
-label_km = Label(text="Km")
-label_km.grid(column=3, row=2)
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
 
-#Entry mile
-entry_mile = Entry(width=10)
-entry_mile.insert(END, string="0")
-entry_mile.grid(column=2, row=1)
+km_result_label = Label(text=0)
+km_result_label.grid(column=1, row=1)
 
-
-#Label
-result = Label(text=0)
-result.grid(column=2, row=2)
+#Entry
+miles_input = Entry(width=7)
+miles_input.insert(END, string="0")
+miles_input.grid(column=1, row=0)
 
 #Button
-def action():
-    res = float(entry_mile.get()) * 1.609
-    result.config(text=res)
-
-calculate_button = Button(text="Calculate", command=action)
-calculate_button.grid(column=2, row=3)
+calculate_button = Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(column=1, row=2)
 window.mainloop()
